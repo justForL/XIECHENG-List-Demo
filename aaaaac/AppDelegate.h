7fdc7d18093bd883a57a2712adcb15fceeae68d0
justForL <layer_james@163.com>
2016-05-22 08:48:55 +0800

    Initial Commit

---
+++ aaaaac/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaaaac
+//
+//  Created by James on 16/5/22.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aaaaac/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaaaac
+//
+//  Created by James on 16/5/22.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
